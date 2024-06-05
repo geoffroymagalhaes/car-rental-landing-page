8026570ba793d41f734a91e7a5110ff790f03509
geoffroymagalhaes <geoffroymagalhaes23@gmail.com>
2024-06-05 12:46:47 +0200

    first commit starting project build components folder with header main footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>POLITIQUE DE CONFIDENTIALITÉ</p>
+      <p>CONDITIONS GÉNÉRALES D'UTILISATION</p>
+      <p>RÉGLEMENTATION - LOI AGEC</p>
+      <p>COOKIE</p>
+      <p>DROITS D'AUTEUR</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "../../assets/logo/logo.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="logo alfa romeo" />
+    </header>
+  );
+};
+
+export default Header;
